756045a2251997d818302c1e0073301978352b91
duck82father <legnaevol84@gmail.com>
2024-02-01 11:04:01 +0900

    02

---
+++ src/test/java/org/kr/ysedu/c402/shop/ImageShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kr.ysedu.c402.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
